fecdb0db727776b4a53e03b7062e220468651d54
Elaine <elainedoud@gmail.com>
2022-11-27 16:56:46 -0500

    commit tree c40829e2ae01ca08ebca0dd03ee369b780c83281

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
 
+    def self.first_movie(movie)
+        Movie.first(movie: movie)
+    end
 end
